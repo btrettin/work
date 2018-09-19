e81e3eeb3c9bdb7cca8e2c61f145c51453fabc7f
Ben Trettin <batrettin@gmail.com>
2018-09-18 20:13:20 -0500

    added parenthessi

--- trettin_benjamin.assignment-1.03/map.c
+++ trettin_benjamin.assignment-1.03/map.c
@@ -564,7 +564,7 @@ int loadGame(){
     for(int i=0; i<21; i++){
       for (int j=0; j<80; j++){
         char temp = mapArray[i][j];
-        if(temp == '.' | temp == '#'){
+        if((temp == '.') | (temp == '#')){
           int value = roomDistanceGrid[i][j].distance;
           value = value % 10;
           printf("%d", value);
